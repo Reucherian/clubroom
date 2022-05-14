c9a597a7336b649705fd66f7d031a44d3e6f0b9a
Aditi Survase <aditi.0296@gmail.com>
2022-05-13 21:19:59 -0400

    init

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,24 @@
 import { API } from 'aws-amplify';
 
-// TODO: create
+// TODO: createRoom
+
+export async function createMeeting(roomID , attendeeName, region) {
+    const joinInfo = await API.graphql(graphqlOperation(createChimeMeeting, {roomid: title, name: attendeeName, region: region }));
+    const joinInfoJson = joinInfo.data.createChimeMeeting;
+    const joinInfoJsonParse = JSON.parse(joinInfoJson.body);
+    return joinInfoJsonParse;
+
+}
+
+
+// meeting id
+
+// title
+// host
+
+// attendee_name
+
+
+
+
+
