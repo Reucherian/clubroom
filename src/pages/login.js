805c781dfd2cc03a09be1589a6c977dee6c16e5a
Reuben Cherian <reuben.cherian@icloud.com>
2022-05-14 01:39:20 -0400

    visual

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Button, Container } from '@mui/material';
+import ClubRoomHeader from '../components/clubRoomHeader';
 const Login = () =>{
     
     return(
     <Container>
+    <ClubRoomHeader></ClubRoomHeader>
     <Button variant="contained" color="secondary">login</Button>
     <Button variant="contained">register</Button>
     </Container>
