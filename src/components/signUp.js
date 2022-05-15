52ca1e6db5d72fa454aea3cf5f8ce7fa0584c530
Aditi Survase <aditi.0296@gmail.com>
2022-05-15 11:02:39 -0400

    signup page updates

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { TextField, styled, Button, CircularProgress } from "@mui/material";
+import { Grid, Paper, TextField, styled, Button, CircularProgress,  Container } from "@mui/material";
 import { useInput } from "./../utils/forms";
 import { Auth } from "aws-amplify";
 import { Link, useNavigate } from "react-router-dom";
+import ClubRoomTitle from "./clubRoomTitle";
+
+
+
 
 const Field = styled(TextField)({
   margin: "10px 0",
@@ -51,40 +55,169 @@ const Signup = () => {
     setLoading(false);
   };
 
-  return (
-    <form
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-      }}
-      onSubmit={handleSignUp}
-    >
-      <h1 style={{ fontSize: "22px", fontWeight: 800 }}>
-        {" "}
-        New Account Registration
-      </h1>
-      <Field label="Name" {...bindName} />
-      <Field label="Email" {...bindEmail} type="email" />
-      <Field label="Password" type="password" {...bindPassword} />
-      <Field
-        label="Confirm Password"
-        type="password"
-        {...bindConfirmPassword}
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        size="large"
-        type="submit"
-        disabled={loading}
-      >
-        {loading && <CircularProgress size={20} style={{ marginRight: 20 }} />}
-        Sign Up
-      </Button>
-      <DLink to="/signin">go to login &rarr;</DLink>
-    </form>
-  );
+//   return (
+//     <form
+//       style={{
+//         display: "flex",
+//         flexDirection: "column",
+//         justifyContent: "space-between",
+//       }}
+//       onSubmit={handleSignUp}
+//     >
+//       <h1 style={{ fontSize: "22px", fontWeight: 800 }}>
+//         {" "}
+//         New Account Registration
+//       </h1>
+//       <Field label="Name" {...bindName} />
+//       <Field label="Email" {...bindEmail} type="email" />
+//       <Field label="Password" type="password" {...bindPassword} />
+//       <Field
+//         label="Confirm Password"
+//         type="password"
+//         {...bindConfirmPassword}
+//       />
+//       <Button
+//         variant="contained"
+//         color="primary"
+//         size="large"
+//         type="submit"
+//         disabled={loading}
+//       >
+//         {loading && <CircularProgress size={20} style={{ marginRight: 20 }} />}
+//         Sign Up
+//       </Button>
+//       <DLink to="/signin">go to login &rarr;</DLink>
+//     </form>
+//   );
+
+return (   
+<Grid container>
+<Grid item md={8}>
+  <Paper
+    style={{
+      background: "#EAE9E6",
+      height: "100vh",
+      position: "relative",
+      zIndex: 0,
+    }}
+    square
+  >
+    <Grid container sx={{ paddingTop: 35 }}>
+
+      <Grid item md={4}>
+      </Grid>
+      <Grid item md={4}>
+        <ClubRoomTitle />
+        <form onSubmit={handleSignUp}>
+          <Grid container>
+            <Grid item md={2}></Grid>
+            <Grid item md={8}>
+              <Grid
+                container
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                  paddingTop: 80,
+                }}
+              >
+                <Field
+                  label={name=== "" ? "name": ""}
+                  type="name"
+                  {...bindName}
+                  inputProps={{ style: {background: "white", borderRadius: 8, fontFamily: "Nunito Sans", fontSize: '1rem'} }}
+                  InputLabelProps={{shrink: false, style:{fontFamily: "Nunito Sans", fontSize: '1rem'}}}
+                />
+                <Field label={email=== "" ? "email": ""} {...bindEmail} type="text" inputProps={{ style: {background: "white", borderRadius: 8, fontFamily: "Nunito Sans", fontSize: '1rem'} }} InputLabelProps={{shrink: false, style:{fontFamily: "Nunito Sans", fontSize: '1rem'}}}/>
+                <Field
+                  label={password=== "" ? "password": ""}
+                  type="password"
+                  {...bindPassword}
+                  inputProps={{ style: {background: "white", borderRadius: 8, fontFamily: "Nunito Sans", fontSize: '1rem'} }}
+                  InputLabelProps={{shrink: false, style:{fontFamily: "Nunito Sans", fontSize: '1rem'}}}
+                />
+                <Field
+                  label={confirmPassword=== "" ? "confirm password": ""}
+                  type="password"
+                  {...bindConfirmPassword}
+                  inputProps={{ style: {background: "white", borderRadius: 8, fontFamily: "Nunito Sans", fontSize: '1rem'} }}
+                  InputLabelProps={{shrink: false, style:{fontFamily: "Nunito Sans", fontSize: '1rem'}}}
+                />
+                <Grid container>
+                  <Grid
+                    item
+                    md={6}
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "space-between",
+                    }}
+                    sx={{ p: 1 }}
+                  >
+                    {/* <Button
+                      variant="contained"
+                      color="secondary"
+                      type="submit"
+                      disabled={loading}
+                    >
+                      {loading && (
+                        <CircularProgress
+                          size={20}
+                          style={{ marginRight: 20 }}
+                        />
+                      )}
+                      Login
+                    </Button> */}
+                  </Grid>
+                  <Grid
+                    item
+                    md={6}
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "space-between",
+                    }}
+                    sx={{ p: 1 }}
+                  >
+                        <Button
+                                variant="contained"
+                                color="primary"
+                                size="large"
+                                type="submit"
+                                disabled={loading}
+                            >
+                                {loading && <CircularProgress size={20} style={{ marginRight: 20 }} />}
+                                Sign Up
+                            </Button>
+
+                          {/* <DLink to="/signin">login page</DLink> */}
+
+                  </Grid>
+                </Grid>
+                <Grid item md={2}></Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+      <Grid item md={4}></Grid>
+    </Grid>
+  </Paper>
+</Grid>
+<Grid item md={4}>
+  <Paper
+    style={{
+      background: "#3FD064",
+      height: "100vh",
+      position: "relative",
+      zIndex: 0,
+    }}
+    square
+  ></Paper>
+</Grid>
+</Grid>
+); 
+
 };
 
 export default Signup;
